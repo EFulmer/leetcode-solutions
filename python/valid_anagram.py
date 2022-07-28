cc2ee242499330b111d1cf759608f1db0b1223b9
Eric Fulmer <2952996+EFulmer@users.noreply.github.com>
2022-07-28 15:05:01 -0400

    non-collections solution for 'Valid Anagram' in Python

--- python/valid_anagram.py
+++ python/valid_anagram.py
@@ -4,3 +4,16 @@ from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return Counter(s) == Counter(t)
+
+# implementing your own counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # short circuit
+        if len(s) != len(t):
+            return False
+        s_counter, t_counter = dict(), dict()
+        for c in s:
+            s_counter[c] = s_counter.get(c, 0) + 1
+        for c in t:
+            t_counter[c] = t_counter.get(c, 0) + 1
+        return t_counter == s_counter
