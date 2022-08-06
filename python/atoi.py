f6e9dc708718a8dc459f715436714126b69b9111
Eric Fulmer <2952996+EFulmer@users.noreply.github.com>
2022-08-06 17:03:26 -0400

    refactoring more

--- python/atoi.py
+++ python/atoi.py
@@ -1,4 +1,9 @@
+INTMIN = -2**31
+INTMAX = 2**31 - 1
+
+
 class Solution:
+
     def myAtoi(self, s: str) -> int:
         # base case: if the string is empty or pure whitespace, return 0
         s = s.lstrip()
@@ -7,30 +12,31 @@ class Solution:
 
         # otherwise, check for a sign:
         if s[0] == "-":
-            negative = True
-            s = s[1:]
+            sign = -1
+            start_index = 1
         elif s[0] == "+":
-            negative = False
-            s = s[1:]
+            sign = 1
+            start_index = 1
         else:
-            negative = False
+            sign = 1
+            start_index = 0
 
         # collect the digits
         digits = []
-        for c in s:
+        cur_index = start_index
+        while cur_index < len(s):
+            c = s[cur_index]
             if not c.isdigit():
                 break
             else:
                 digits.append(c)
+                cur_index = cur_index + 1
 
-        # and sum up the result:
+        # and sum 'em up:
         result = 0
-        for position, digit in zip(range(0, len(digits)), reversed(digits)):
+        for position, digit in zip(range(len(digits)-1, -1, -1), digits):
             result = result + (10**position) * int(digit)
-        if negative:
-            result = -result
-        if result < -2**31:
-            result = -2**31
-        elif result > 2**31 - 1:
-            result = 2**31 - 1
+
+        result = result * sign
+        result = min(max(result, INTMIN), INTMAX)
         return result
