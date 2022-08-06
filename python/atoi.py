0aae0a2b798f2efee5f0dd033ebd0a56c3aaed9b
Eric Fulmer <2952996+EFulmer@users.noreply.github.com>
2022-08-06 16:38:28 -0400

    comment solution; part 1 of refactoring

--- python/atoi.py
+++ python/atoi.py
@@ -1,9 +1,11 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
+        # base case: if the string is empty or pure whitespace, return 0
         s = s.lstrip()
         if not s:
             return 0
-        digits = []
+
+        # otherwise, check for a sign:
         if s[0] == "-":
             negative = True
             s = s[1:]
@@ -13,11 +15,15 @@ class Solution:
         else:
             negative = False
 
+        # collect the digits
+        digits = []
         for c in s:
             if not c.isdigit():
                 break
             else:
                 digits.append(c)
+
+        # and sum up the result:
         result = 0
         for position, digit in zip(range(0, len(digits)), reversed(digits)):
             result = result + (10**position) * int(digit)
