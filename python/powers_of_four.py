fa9a9ae32751b7fea16b49b768d31e2cb1abe401
Eric Fulmer <2952996+EFulmer@users.noreply.github.com>
2022-08-22 15:26:52 -0400

    first solution for 'Powers of Four' in Python

---
+++ python/powers_of_four.py
@@ -0,0 +1,34 @@
+POWERS_OF_FOUR = {4**x for x in range(0, 16)}  # 4**16 > 2**31 - 1
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        # We can solve this without logarithms
+        # by looking at the binary representations of powers of four:
+        # In [13]: powers_of_four = [4**x for x in range(0, 20)]
+        # In [14]: [bin(x) for x in powers_of_four]
+        # Out[14]:
+        # ['0b1',
+        #  '0b100',
+        #  '0b10000',
+        #  '0b1000000',
+        #  '0b100000000',
+        #  '0b10000000000',
+        #  '0b1000000000000',
+        #  '0b100000000000000',
+        #  '0b10000000000000000',
+        #  '0b1000000000000000000',
+        #  '0b100000000000000000000',
+        #  '0b10000000000000000000000',
+        #  '0b1000000000000000000000000',
+        #  '0b100000000000000000000000000',
+        #  '0b10000000000000000000000000000',
+        #  '0b1000000000000000000000000000000',
+        #  '0b100000000000000000000000000000000',
+        #  '0b10000000000000000000000000000000000',
+        #  '0b1000000000000000000000000000000000000',
+        #  '0b100000000000000000000000000000000000000']
+        # In [16]: [len(bin(x)[2:]) for x in powers_of_four]
+        # Out[16]: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39]
+        # even-length binary string, 1 with an odd number of 0's following it
+        # so we can check by seeing if n follows that rule
+        return n in POWERS_OF_FOUR
