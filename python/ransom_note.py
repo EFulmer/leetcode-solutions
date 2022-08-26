47f52221073102f7748d00313cfa69e5c1a10180
Eric Fulmer <2952996+EFulmer@users.noreply.github.com>
2022-08-25 21:35:52 -0400

    no collections solution for 'Ransom Note' in Python

--- python/ransom_note.py
+++ python/ransom_note.py
@@ -6,3 +6,18 @@ class Solution:
         note = Counter(ransomNote)
         magazine = Counter(magazine)
         return magazine >= note
+
+
+# second solution
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        tally = [0 for _ in range(26)]
+        for char in magazine:
+            tally[ord(char) - 97] += 1
+
+        for char in ransomNote:
+            i = ord(char) - 97
+            if tally[i] <= 0:
+                return False
+            tally[i] -= 1
+        return True<Paste>
