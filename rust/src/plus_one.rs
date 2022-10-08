f9343b5e7bd767715f32317886eabe4ee0b5fce0
Eric Fulmer <2952996+EFulmer@users.noreply.github.com>
2022-10-08 16:12:23 -0400

    clean up solution for 'Plus One' in Rust

--- rust/src/plus_one.rs
+++ rust/src/plus_one.rs
@@ -6,18 +6,12 @@ impl Solution {
         // Keep carrying until you don't need to carry again.
 
         let mut result = digits.clone();
-        let mut n = result.len() - 1;
-        let mut carry = 0;
-        result[n] += 1;
-        if result[n] == 10 {
-            result[n] = 0;
-            carry = 1;
-        } else if 0 <= result[n] && result[n] <= 9 {
-            carry = 0;
-        } else {
-            panic!("Somehow the current digit is not in the range [1, 9] (should never happen)");
-        }
+        let mut n = result.len();
+        let mut carry = 1;
         for i in (0..n).rev() {
+            if carry == 0 {
+                break;
+            }
             result[i] += carry;
             if result[i] == 10 {
                 result[i] = 0;
@@ -28,8 +22,8 @@ impl Solution {
                 panic!("Somehow the current digit is not in the range [1, 9] (should never happen)");
             }
         }
+        // If there's still a carry after going through all the digits we have been given, we need to add a 1 to the start.
         if carry == 1 {
-            // prepend 1 to the start of the vector
             result[0] = 0;
             result.insert(0, 1);
         }
