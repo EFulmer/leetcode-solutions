9d387d08fb67ae45e29d05e587b666ffa114916a
Eric Fulmer <2952996+EFulmer@users.noreply.github.com>
2023-01-06 18:22:34 -0500

    solution for 'Maximum Ice Cream Bars' in Python

---
+++ python/maximum_ice_cream_bars.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        # hard code some early termination cases
+        if coins > sum(costs):
+            return len(costs)
+        elif coins < min(costs):
+            return 0
+        result = 0
+        for i in sorted(costs):
+            if i > coins:
+                break
+            else:
+                result += 1
+                coins -= i
+        return result
