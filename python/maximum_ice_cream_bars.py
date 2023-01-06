455972fbc373a48c2172355e09ea7d22eb9308dc
Eric Fulmer <2952996+EFulmer@users.noreply.github.com>
2023-01-06 18:25:11 -0500

    fix bug in Maximum Ice Cream Bars

--- python/maximum_ice_cream_bars.py
+++ python/maximum_ice_cream_bars.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
         # hard code some early termination cases
-        if coins > sum(costs):
+        if coins >= sum(costs):
             return len(costs)
         elif coins < min(costs):
             return 0
