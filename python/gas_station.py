f14c77f99f12e4e2de9d8b26a3df072f1c69ba78
Eric Fulmer <2952996+EFulmer@users.noreply.github.com>
2023-01-07 18:41:41 -0500

    add itertools solution for 'Gas Station' in Python

--- python/gas_station.py
+++ python/gas_station.py
@@ -31,3 +31,24 @@ class Solution:
             else:
                 idx = (idx + 1) % n
         return True
+
+
+import itertools
+
+
+class Solution2:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # Since we start with 0 gas, if there's not enough gas, insta-lose:
+        if sum(cost) > sum(gas):
+            return -1
+        # Adapted from another sol:
+        # Since there's only one solution, we need to end at the station which drains the most gas total, because you can't get from this staiton to the next
+        # So compute how much gas it takes to get from each station to the next, starting with an empty tank
+        cost_to_next_station = list(
+            itertools.accumulate((g-c for g, c in zip(gas, cost)), initial=0)
+        )
+        # If you can end the circuit without an empty gas tank, then the answer is the station with the greatest deficit.
+        if cost_to_next_station[-1] >= 0:
+            return cost_to_next_station.index(min(cost_to_next_station))
+        else:
+            return -1
