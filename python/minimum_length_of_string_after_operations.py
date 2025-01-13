230bfc78d670bb024a200ae2291ee3572fb0716d
Eric R. Fulmer <2952996+EFulmer@users.noreply.github.com>
2025-01-13 10:48:43 -0500

    Revise Python solution for '3223. Minimum Length of String After Operations'

--- python/minimum_length_of_string_after_operations.py
+++ python/minimum_length_of_string_after_operations.py
@@ -3,10 +3,15 @@ class Solution:
         if len(s) < 3:
             return len(s)
         hist = collections.Counter(s)
-        done = False
-        while not done:
-            for k, v in hist.items():
-                if v > 2:
-                    hist[k] -= 2
-            done = all(v < 3 for v in hist.values())
+        # Repeatedly subtracting 2 occurrences of a character will
+        # either result in its final count being 2
+        # (when original count is even)
+        # or 1 (when its original count is odd)
+        # We can skip the "steps" using this fact.
+        for k, v in hist.items():
+            if v > 2:
+                if v % 2 == 0:
+                    hist[k] = 2
+                else:
+                    hist[k] = 1
         return sum(hist.values())
