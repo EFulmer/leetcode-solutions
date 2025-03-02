70d071b3d7a12bebf9b7bddba1fa263786fb2066
Eric R. Fulmer <2952996+EFulmer@users.noreply.github.com>
2025-03-02 09:54:11 -0500

    Additional solution to '67. Add Binary' in Python

--- python/67_add_binary.py
+++ python/67_add_binary.py
@@ -6,3 +6,39 @@ class Solution:
         a_int = int(a, 2)
         b_int = int(b, 2)
         return bin(a_int+b_int)[2:]
+
+
+# Actual arithmetic solution:
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a_length, b_length = len(a), len(b)
+        if a_length > b_length:
+            b = ('0' * (a_length - b_length)) + b
+        elif a_length < b_length:
+            a = ('0' * (b_length - a_length)) + a
+        result = []
+        carry = None
+        for c1, c2 in zip(reversed(a), reversed(b)):
+            # Add carry, c1, and c2
+            if c1 == '1' and c2 == '1':
+                if carry:
+                    carry = '1'
+                    result.append('1')
+                else:
+                    carry = '1'
+                    result.append('0')
+            elif c1 == '0' and c2 == '0':
+                if carry:
+                    carry = None
+                    result.append('1')
+                else:
+                    result.append('0')
+            else:  # a zero and a one
+                if carry:
+                    carry = '1'
+                    result.append('0')
+                else:
+                    result.append('1')
+        if carry == '1':
+            result.append(carry)
+        return "".join(reversed(result))
